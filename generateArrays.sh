bc6ec087311a2ae0218fd3b62e2075c211898e46
TED_NICKELS <theo.n.business@gmail.com>
2025-05-10 12:49:57 -0600

    wrote explanations of my shell scripts, deleted the AI dribble (benchmark.sh)  in favor of my simple human bash scripts (goodbenchmark.sh). if you dont have permission, run chmod +x (filename.sh) to give yourself execution rights

--- generateArrays.sh
+++ generateArrays.sh
@@ -2,25 +2,36 @@
 
 
 OUTPUT_DIR="intarrays"
+OUTPUT_DIR2="floatarrays"
 mkdir -p "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR2"
 
-START=20
+START=29
 END=30
 
-# generate arrays from 2^20 to 2^30
+# generate arrays from 2^start to 2^end (IF YOU JUST WANT TO GENERATE ALL, CHANGE START TO 20 AND END TO 30. I RECCOMEND YOU USE MINE FROM DRIVE AND ONLY RUN THIS FOR 29,30)
 for ((i=START; i<=END; i++)); do
     SIZE=$((2 ** i))
-    FILENAME="$OUTPUT_DIR/array_$i.npy"
+    FILENAME="$OUTPUT_DIR/array_2^$i.npy"
+    FILENAME2="$OUTPUT_DIR2/array_2^$i.npy"
 
     echo "Generating array of size 2^$i ($SIZE)..."
+
+# THESE SIMPLY RUN THE QUOTED CODE. FILES ARE SAVED TO THEIR RESPECTIVE DIRECTORIES IMPLICITLY BY ASSIGNATION OF FILENAME
 
     python3 -c "
 import numpy as np
 a = np.random.rand($SIZE).astype(np.int64)
 np.save('$FILENAME', a)
 "
+    python3 -c "
+import numpy as np
+a = np.random.rand($SIZE).astype(np.float64)
+np.save('$FILENAME2', a)
+"
 
     echo "Saved to $FILENAME"
+    echo "Saved to $FILENAME2"
 done
 
-echo "arrays generated in the '$OUTPUT_DIR' directory."
+echo "arrays generated in the '$OUTPUT_DIR' and '$OUTPUT_DIR2' directory."
--- goodBenchmark.sh
+++ goodBenchmark.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
+
+# IF YOU ONLY WANT TO TEST 29 AND 30, JUST CHANGE THE FOR LOOP TO 29..30
 for i in {20..29}; do
-    file="intarrays/array_$i.npy"
+    file="intarrays/array_$i.npy" # NAMED BY MY OTHER SHELL SCRIPT. TO TEST YOUR CODE WITH FLOATS, SIMPLY CHANGE intarrays/ TO floatarrays/
     echo "sorting $file"
-    python3 three_way_time.py "$file"
+    python3 three_way_time.py "$file" # IF YOU WANT TO TEST YOUR CODE, REPLACE MY FILENAME (three_way_time.py) WITH YOURS 
 done 
